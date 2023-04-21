7f6380d01614c8462dc1d556bf522d38e94746bd
l4b13 <72724714+djwbhr@users.noreply.github.com>
2023-04-21 15:55:16 +0900

    first

---
+++ static/abi.js
@@ -0,0 +1,191 @@
+let abi = [
+	{
+		"inputs": [
+			{
+				"internalType": "string",
+				"name": "fio",
+				"type": "string"
+			},
+			{
+				"internalType": "string",
+				"name": "snils",
+				"type": "string"
+			},
+			{
+				"internalType": "string",
+				"name": "dep",
+				"type": "string"
+			},
+			{
+				"internalType": "string",
+				"name": "number",
+				"type": "string"
+			},
+			{
+				"internalType": "string",
+				"name": "issue_date",
+				"type": "string"
+			}
+		],
+		"name": "AddDiplom",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "string",
+				"name": "fio",
+				"type": "string"
+			},
+			{
+				"internalType": "string",
+				"name": "birthday",
+				"type": "string"
+			},
+			{
+				"internalType": "string",
+				"name": "snils",
+				"type": "string"
+			}
+		],
+		"name": "AddStudent",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "string",
+				"name": "fio",
+				"type": "string"
+			},
+			{
+				"internalType": "string",
+				"name": "snils",
+				"type": "string"
+			}
+		],
+		"name": "DeleteStudent",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "string",
+				"name": "new_fio",
+				"type": "string"
+			},
+			{
+				"internalType": "string",
+				"name": "old_fio",
+				"type": "string"
+			},
+			{
+				"internalType": "string",
+				"name": "snils",
+				"type": "string"
+			}
+		],
+		"name": "UpdateStudent",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "string",
+				"name": "fio",
+				"type": "string"
+			},
+			{
+				"internalType": "string",
+				"name": "snils",
+				"type": "string"
+			}
+		],
+		"name": "GetDiploma",
+		"outputs": [
+			{
+				"components": [
+					{
+						"internalType": "string",
+						"name": "number",
+						"type": "string"
+					},
+					{
+						"internalType": "string",
+						"name": "department",
+						"type": "string"
+					},
+					{
+						"internalType": "string",
+						"name": "issue_date",
+						"type": "string"
+					},
+					{
+						"internalType": "bool",
+						"name": "is_active",
+						"type": "bool"
+					}
+				],
+				"internalType": "struct Diplomas.Diplom[]",
+				"name": "",
+				"type": "tuple[]"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "string",
+				"name": "fio",
+				"type": "string"
+			},
+			{
+				"internalType": "string",
+				"name": "snils",
+				"type": "string"
+			}
+		],
+		"name": "GetStudent",
+		"outputs": [
+			{
+				"components": [
+					{
+						"internalType": "string",
+						"name": "fio",
+						"type": "string"
+					},
+					{
+						"internalType": "string",
+						"name": "birthday",
+						"type": "string"
+					},
+					{
+						"internalType": "bool",
+						"name": "exists",
+						"type": "bool"
+					},
+					{
+						"internalType": "uint256[]",
+						"name": "diplomas_ind",
+						"type": "uint256[]"
+					}
+				],
+				"internalType": "struct Diplomas.Student",
+				"name": "",
+				"type": "tuple"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	}
+]
